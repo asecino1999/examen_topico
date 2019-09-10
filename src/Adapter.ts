f3ef88343a08b272543ac51b4070c4a092873e4f
asecino1999 <asecino1999@gmail.com>
2019-09-10 13:19:04 -0500

    first

---
+++ src/Adapter.ts
@@ -0,0 +1,119 @@
+
+
+export class Matriz {
+    matriz:number[][];
+    constructor(x:number, y:number ){
+        this.matriz=[];
+        for(var i: number = 0; i < x; i++) {
+            this.matriz[i] = [];
+            for(var j: number = 0; j< y; j++) {
+                this.matriz[i][j] = i+j;
+            }
+        }
+    }
+    getElement(x:number,y:number):number{
+        try {
+            return this.matriz[x][y];    
+        } catch (error) {
+            return -Infinity;
+        }
+        
+    }
+    setElement(x:number,y:number,value:number):number{
+        try {
+            this.matriz[x][y]=value;
+            return value;
+        } catch (error) {
+            return -Infinity;
+        }
+    }
+}
+
+
+
+
+
+
+export class  Vector {
+    vector:Array<number>;
+    constructor(y:number){
+        this.vector=[];
+        for(var j: number = 0; j< y; j++) {
+            this.vector[j] = j;
+        }
+    }
+    getElement(y:number):number{
+        try {
+            return this.vector[y];    
+        } catch (error) {
+            return -Infinity;
+        }
+        
+    }
+    setElement(y:number,value:number):number{
+        try {
+            this.vector[y]=value;
+            return value;
+        } catch (error) {
+            return -Infinity;
+        }
+    }
+    length():number{
+        return this.vector.length;
+    }
+}
+abstract class AbstractAdapter{
+    abstract filas:number;
+    abstract columnas:number;
+    abstract getElement(x:number,y:number):number;
+    abstract setElement(x:number,y:number,value:number):number;
+
+}
+export class Adapter extends AbstractAdapter{
+    vector:Vector;
+    filas:number;
+    columnas:number;
+    constructor(filas:number,columnas:number,vector?:Vector){
+        super();
+        if(typeof(vector)!=="undefined")
+            this.vector=vector;
+        else
+            this.vector=new Vector(filas*columnas);
+        
+        this.filas=filas;
+        this.columnas=columnas;
+
+        if(filas*columnas>this.vector.length()){
+            console.log("error logitud ");
+            this.filas=this.vector.length();
+            this.columnas=1;
+        }
+    }
+    
+    getElement(x:number,y:number):number{
+        if(this.columnas>y && this.filas>x  ){
+            return this.vector.getElement(x*this.columnas+y);
+        } else {
+            console.log("error de posicoin");
+            return -Infinity;
+        }
+    };
+    setElement(x:number,y:number,value:number):number{
+        if(this.columnas>y && this.filas>x  ){
+            return this.vector.setElement(x*this.columnas+y,value);
+        } else {
+            console.log("error de posicoin");
+            return -Infinity;
+        }
+    };
+}
+
+
+
+//console.log((new Matriz(5,5)).getElement(6,6));
+//console.log((new Matriz(5,5)).setElement(4,4,7));
+//var adapter:Adapter=new Adapter(3,4,new Vector(12));
+//console.log(adapter.setElement(3,2,80));
+//console.log(adapter.vector);
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,118 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Vector,Adapter} from './Adapter'
+//import logo from './logo.svg';
+//import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// ejemplo pasar un js como un objeto 
+//var dia: Pat.Dimante = new Pat.Dimante(
+//  { max: Infinity, descuento: 40, millaje: 5 } as Pat.Rango, "Diamante"
+//  );
+interface IState {
+  color:string;
+  fil:number;
+  col:number;
+  adap:Adapter;
+  tempFil:number;
+  tempCol:number;
+}
+class App extends React.Component<{}, IState> {
+  constructor(props:any){
+    super(props);
+    var fil=5;
+    var col=3;
 
-const App: React.FC = () => {
+    this.state={
+      color:"red",
+      fil:fil,
+      col:col,
+      tempCol:col,
+      tempFil:fil,
+      adap:new Adapter(fil,col)
+    }
+  }
+  setG(){
+    this.setState({color:"green"} );
+  }
+
+  setValue(x:number,y:number,ev:React.ChangeEvent<HTMLInputElement>){
+    var adap:Adapter=this.state.adap;
+    var value:number=parseFloat(ev.target.value);
+    adap.setElement(x,y,value);
+    this.setState({});
+  }
+  setTempFil(x:number,ev:React.ChangeEvent<HTMLInputElement>){
+
+  }
+
+  setSize(){
+
+  }
+
+  render(){
+  var col:Array<number>=[];
+  for (let index = 0; index < this.state.col; index++) {
+    col.push(index);
+  }
+  var fil=[]; 
+  for (let index = 0; index < this.state.fil; index++) {
+    fil.push(index);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={ {backgroundColor: this.state.color}}>
+      <button onClick={()=>this.setG()}>set size </button>
+      <div> numero de filas <input type="number" value={this.state.fil} /></div>
+      <div> numero de columnas <input type="number" value={this.state.col} /></div>
+
+      <Container>
+        
+      {
+        fil.map((element, index) => {
+          //console.log("itera "+index)
+          return(
+            <Row key={"index"+index} >
+              {
+                col.map((element,index2) => {
+                  //console.log("itera 2 "+index2)
+                  return(<Col key={"index"+index+"index2"+index2}>
+                    <input type="number" onChange={(ev)=>this.setValue(index,index2,ev)} value={this.state.adap.getElement(index,index2)}></input>
+                  </Col>)
+                  ;
+                })
+              }
+          </Row>
+          )
+        })
+      }
+        
+      </Container>
+      <Container>
+        
+        {
+          fil.map((element, index) => {
+            //console.log("itera "+index)
+            return(
+              <Row key={"2index"+index} >
+                {
+                  col.map((element,index2) => {
+                    //console.log("itera 2 "+index2)
+                    return(<Col key={"2index"+index+"index2"+index2}>
+                     {this.state.adap.getElement(index,index2)}
+                    </Col>)
+                    ;
+                  })
+                }
+            </Row>
+            )
+          })
+        }
+          
+        </Container>
     </div>
-  );
+  );}
 }
 
 export default App;
